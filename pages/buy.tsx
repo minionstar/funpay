e53e021dc3380ce7e73ed5a26ad2a7d2d74c1a82
MinionStar <94663844+minionstar@users.noreply.github.com>
2022-12-15 07:54:05 -0500

    Update buy.tsx

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -30,7 +30,7 @@ const ChatItem = () => {
           Anakin
           <time className="text-xs opacity-50">12:46</time>
         </div>
-        <div className="chat-bubble">I hate you!</div>
+        <div className="chat-bubble">I love you!</div>
         <div className="chat-footer opacity-50">Seen at 12:46</div>
       </div>
     </>
